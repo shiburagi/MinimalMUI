340b4a9955f0441e31b29905aab2387c0cfc1b15
Shiburagi <tr32010@gmail.com>
2019-01-11 03:56:18 +0800

    Updates

---
+++ precache-manifest.c42ea6b6d9fdc56d0b2690307caf534c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e439927021c13a18becc1900b33a63",
+    "url": "/MinimalMUI/static/media/user1.f3e43992.jpg"
+  },
+  {
+    "revision": "b9fbb3426179f148e3b013082dc2453e",
+    "url": "/MinimalMUI/static/media/banner1.b9fbb342.jpg"
+  },
+  {
+    "revision": "870b7c1696945efb0317",
+    "url": "/MinimalMUI/static/js/runtime~main.870b7c16.js"
+  },
+  {
+    "revision": "4c2b7b95e878ddcfaf24",
+    "url": "/MinimalMUI/static/js/main.4c2b7b95.chunk.js"
+  },
+  {
+    "revision": "ff7f6d6e97723dd0059c",
+    "url": "/MinimalMUI/static/js/1.ff7f6d6e.chunk.js"
+  },
+  {
+    "revision": "4c2b7b95e878ddcfaf24",
+    "url": "/MinimalMUI/static/css/main.85c3d6e7.chunk.css"
+  },
+  {
+    "revision": "0b954e0e7ec7b50f512a2410c7f97a0d",
+    "url": "/MinimalMUI/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MinimalMUI/precache-manifest.508d701c1805e8a17024177577acefe1.js"
+  "/MinimalMUI/precache-manifest.c42ea6b6d9fdc56d0b2690307caf534c.js"
 );
 
 workbox.clientsClaim();
